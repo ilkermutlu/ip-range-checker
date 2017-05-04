e1a2a8a75bdd6f629701a9470007f07ef920bc77
Ilker Mutlu <ilker.mutlu@gmail.com>
2017-05-04 23:07:18 +0300

    Support dashed ranges.

--- src/Checker.php
+++ src/Checker.php
@@ -26,6 +26,10 @@ class Checker
         if (is_array($range)) {
             $start = IPAddress::fromIPString($range[0]);
             $end   = IPAddress::fromIPString($range[1]);
+        } else if (strpos($range, '-')) {
+            $ranges = explode('-', $range);
+            $start = IPAddress::fromIPString($ranges[0]);
+            $end   = IPAddress::fromIPString($ranges[1]);
         } else {
             $start = IPAddress::fromIPString(str_replace('*', '0', $range));
             $end   = IPAddress::fromIPString(str_replace('*', '255', $range));
